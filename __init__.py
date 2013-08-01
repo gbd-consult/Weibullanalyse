1eedb5ef004a4984045544fd3899c305f45fd27a
dassau <dassau@gbd-consult.de>
2013-08-01 13:57:19 +0200

    make plugin start under QGIS 2.0

--- __init__.py
+++ __init__.py
@@ -21,43 +21,8 @@
  This script initializes the plugin, making it known to QGIS.
 """
 
-# load valuetool class from file valuetool.py
-
-def name(): 
-    return 'Weibullauswertung'
-
-def description():
-    return 'Berechnung und Darstellung einer Weibullauswertung für Windenergiestandorte' 
-
-def version(): 
-    return 'Version 0.1'
-
-def qgisMinimumVersion():
-    return '2.0'
-
-def author():
-    return "dassau@gbd-consult.de"
-
-def email():
-    return "dassau@gbd-consult.de"
-
-def category():
-    return 'Raster'
-
-def authorName():
-    return 'Otto Dassau'
-
-def icon():
-    return "icon.png"
-
 def classFactory(iface):
     # load WeibullAnalyse class from file WeibullAnalyse
     from weibullanalyse import ValueTool
     return ValueTool(iface)
-
-## später ueberfluessig
-#def classFactory(iface): 
-#    from valuetool import ValueTool
-#    return ValueTool(iface)
-
 
